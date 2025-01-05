84a87fb9c229cd542f7a63f36e2f9cf43bef6719
Bruno Henrique Paiva <bruno@bruno.codes>
2025-01-05 01:34:22 -0300

    refactor: move config to own module and visibility changes

--- src/lib.rs
+++ src/lib.rs
@@ -1,55 +1,25 @@
-use std::{error::Error, sync::Arc};
+use std::sync::Arc;
 
+use anyhow::Result;
 use askama_axum::Template;
 use axum::{
     routing::{get, post},
     Router,
 };
 use compat::maloja::maloja_new_scrobble;
-use deadpool_postgres::{tokio_postgres::NoTls, Config, CreatePoolError, Pool, Runtime};
+use config::AppConfig;
+use deadpool_postgres::{tokio_postgres::NoTls, Config, Pool, Runtime};
 
 mod compat;
+pub mod config;
 mod db;
 mod extractor;
 
-pub struct AppConfig {
-    addr: String,
-    db_host: String,
-    db_port: u16,
-    db_name: String,
-    db_user: String,
-    db_password: String,
-}
-
-impl AppConfig {
-    pub fn from_env() -> Result<Self, Box<dyn Error>> {
-        let addr = std::env::var("ADDRESS").unwrap_or("0.0.0.0:3000".to_string());
-        let db_host = std::env::var("DB_HOST").unwrap_or("0.0.0.0".to_string());
-        // Of course there is a better way to do this... Just prototyping for now.
-        let db_port: u16 = std::env::var("DB_PORT")
-            .unwrap_or("5432".to_string())
-            .parse()
-            .unwrap_or(5432);
-        let db_name = std::env::var("DB_NAME").unwrap_or("chlorine".to_string());
-        let db_user = std::env::var("DB_USER").unwrap_or("user".to_string());
-        let db_password = std::env::var("DB_PASSWORD").unwrap_or("password".to_string());
-
-        Ok(Self {
-            addr,
-            db_host,
-            db_port,
-            db_name,
-            db_user,
-            db_password,
-        })
-    }
-}
-
-pub struct AppState {
+struct AppState {
     pub pool: Pool,
 }
 
-pub async fn start_server(config: AppConfig) -> Result<(), Box<dyn Error>> {
+pub async fn start_server(config: AppConfig) -> Result<()> {
     tracing_subscriber::fmt()
         .with_max_level(tracing::Level::DEBUG)
         .init();
@@ -72,7 +42,7 @@ pub async fn start_server(config: AppConfig) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-fn create_db_pool(config: &AppConfig) -> Result<Pool, CreatePoolError> {
+fn create_db_pool(config: &AppConfig) -> Result<Pool> {
     let mut cfg = Config::new();
     cfg.host = Some(config.db_host.clone());
     cfg.port = Some(config.db_port.clone());
@@ -80,7 +50,7 @@ fn create_db_pool(config: &AppConfig) -> Result<Pool, CreatePoolError> {
     cfg.user = Some(config.db_user.clone());
     cfg.password = Some(config.db_password.clone());
 
-    cfg.create_pool(Some(Runtime::Tokio1), NoTls)
+    Ok(cfg.create_pool(Some(Runtime::Tokio1), NoTls)?)
 }
 
 async fn get_index() -> IndexTemplate {
