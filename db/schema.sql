5f7daa2690e72facc8d5b73debda930fd84e0399
Bruno Henrique Paiva <bruno@bruno.codes>
2025-09-24 02:00:20 -0300

    chore: new database schema

--- db/schema.sql
+++ db/schema.sql
@@ -12,8 +12,6 @@ CREATE TABLE track (
 	id INT NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
 	title TEXT NOT NULL,
 	slug TEXT NOT NULL,
-	-- this is a dumb way to store artists I think, need to think more about it
-	main_artist_id INT NOT NULL REFERENCES artist,
 	length INT, -- allow null values for now
 	UNIQUE (slug)
 );
@@ -21,10 +19,37 @@ CREATE TABLE track (
 CREATE TABLE track_artist (
 	track_id INT NOT NULL REFERENCES track,
 	artist_id INT NOT NULL REFERENCES artist,
+	credited_as TEXT,
+	join_phrase TEXT,
+	artist_order INT NOT NULL,
 	PRIMARY KEY (track_id, artist_id)
+);
+
+CREATE TABLE album (
+	id INT NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	title TEXT NOT NULL,
+	slug TEXT NOT NULL,
+	UNIQUE (slug)
+);
+
+CREATE TABLE album_track (
+	album_id INT NOT NULL REFERENCES album,
+	track_id INT NOT NULL REFERENCES track,
+	track_number INT NOT NULL,
+	PRIMARY KEY (album_id, track_id)
+);
+
+CREATE TABLE album_artist (
+	album_id INT NOT NULL REFERENCES album,
+	artist_id INT NOT NULL REFERENCES artist,
+	credited_as TEXT,
+	join_phrase TEXT,
+	artist_order INT NOT NULL,
+	PRIMARY KEY (album_id, artist_id)
 );
 
 CREATE TABLE scrobble (
 	utc_date_time TIMESTAMPTZ NOT NULL PRIMARY KEY,
-	track_id INT NOT NULL REFERENCES track
+	track_id INT NOT NULL REFERENCES track,
+	album_id INT REFERENCES album
 );
