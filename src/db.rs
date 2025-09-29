5e2709471164a5cb79902cc97a99e3c5811222f6
Bruno Henrique Paiva <bruno@bruno.codes>
2025-09-29 00:26:46 -0300

    refactor: move db pool creation to db module

--- src/db.rs
+++ src/db.rs
@@ -1,4 +1,20 @@
+use anyhow::Result;
+use deadpool_postgres::{Config, Pool, Runtime, tokio_postgres::NoTls};
+
+use crate::config::AppConfig;
+
 mod album;
 mod artist;
 pub mod scrobble;
 mod track;
+
+pub fn create_pool(config: &AppConfig) -> Result<Pool> {
+    let mut cfg = Config::new();
+    cfg.host = Some(config.db_host.clone());
+    cfg.port = Some(config.db_port.clone());
+    cfg.dbname = Some(config.db_name.clone());
+    cfg.user = Some(config.db_user.clone());
+    cfg.password = Some(config.db_password.clone());
+
+    Ok(cfg.create_pool(Some(Runtime::Tokio1), NoTls)?)
+}
